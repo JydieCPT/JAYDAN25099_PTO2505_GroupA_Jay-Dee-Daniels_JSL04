8e4c529457f082f8dfcdb74838d3ba9ee9dd83aa
Jay-Dee Daniels <jydiecpt@gmail.com>
2025-07-20 15:37:33 +0200

    Added initial modal code to file.

--- modal.js
+++ modal.js
@@ -1,1 +1,18 @@
+const modalBackdrop = document.getElementById('task-modal-backdrop');
+const modalTitle = document.getElementById('modal-title');
+const modalDesc = document.getElementById('modal-desc');
+const modalStatus = document.getElementById('modal-status');
+const closeModalBtn = document.getElementById('close-modal');
 
+document.querySelectorAll('.task').forEach(task => {
+  task.addEventListener('click', () => {
+    modalTitle.value = task.getAttribute('data-title');
+    modalDesc.value = task.getAttribute('data-description');
+    modalStatus.value = task.getAttribute('data-status');
+    modalBackdrop.classList.remove('hidden');
+  });
+});
+
+closeModalBtn.addEventListener('click', () => {
+  modalBackdrop.classList.add('hidden');
+});
